0bc319699223c88243a9c1a08e910ea6560c064c
47chen <shihchichen1010@vt.edu>
2022-01-22 21:19:30 -0500

    add Env for PORT and URL

--- server/index.js
+++ server/index.js
@@ -2,22 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://shihchichen1010:Ben86317154@cluster0.oqjph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://shihchichen1010:Ben86317154@cluster0.oqjph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5001;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
   )
