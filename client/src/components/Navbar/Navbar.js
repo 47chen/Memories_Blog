75813fc149ec9fae3727dd609777611513388c8b
47chen <shihchichen1010@vt.edu>
2022-01-27 14:35:41 -0500

    Fix issue: 'refreshing page to get login profile' by using useEffect and useLocation

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { AppBar, Avatar, Button, Typography, Toolbar } from "@material-ui/core";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
@@ -10,6 +10,7 @@ const Navbar = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const logout = () => {
     dispatch({ type: "LOGOUT" });
@@ -23,7 +24,7 @@ const Navbar = () => {
     //JWT.. later on for manual sign up
 
     setUser(JSON.parse(localStorage.getItem("profile")));
-  }, []);
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
