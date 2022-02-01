3160a7126cf0e31ede8b3cc8f13d01da1253b01b
47chen <shihchichen1010@vt.edu>
2022-02-01 17:36:11 -0500

    users route typo fixed to be same as frontend api route

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 
 // const CONNECTION_URL =
 //   "mongodb+srv://shihchichen1010:Ben86317154@cluster0.oqjph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
